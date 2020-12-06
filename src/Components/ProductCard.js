28a84557d19ef19c3237028ecf0246f3cf6700c6
Nicolas Terra Maia <nicolasterramaia@gmail.com>
2020-12-06 19:05:40 -0300

    product card and product listcreated

---
+++ src/Components/ProductCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ProductCard = ({ product }) => {
+	return (
+		<div className='border mb-4 rounded overflow-hidden'>
+			<Link to={`/products/${product.id}`}>
+				<div style={{ backgroundImage: `url('${product.images[0].imageUrl}')` }} className='w-full h-64 bg-blue bg-cover'></div>
+			</Link>
+			<div className='p-3'>
+				<h3 className='font-bold text-xl mb-3'>
+					<Link to={`/products/${product.id}`}>{product.name}</Link>
+				</h3>
+				<div className='font-bold mb-3'>${product.price}</div>
+				<div className='mb-3'>{product.description}</div>
+				<Link to={`/products/${product.id}`} className='bg-blue-500 text-white p-2 flex justify-center w-full'>
+					View
+				</Link>
+			</div>
+		</div>
+	);
+};
+
+export default ProductCard;
--- src/View/Home.js
+++ src/View/Home.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import Loader from '../Components/Loader';
+import ProductCard from '../Components/ProductCard';
 
 const Home = () => {
+	const [products, setProducts] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [fetchError, setFetchError] = useState(false);
+
+    
+	useEffect(() => {
+        const url = `https://5fccf355603c0c00164873c8.mockapi.io/products/?page=1&limit=10`;
+		const fetchProductList = async () => {
+			setLoading(true);
+			try {
+				const response = await axios.get(url);
+				setLoading(false);
+				setProducts(response.data);
+				setFetchError(false);
+			} catch (error) {
+				setLoading(false);
+				setProducts(null);
+				setFetchError(true);
+			}
+		};
+
+		fetchProductList();
+	}, []);
+
+	const renderContent = () => {
+		if (loading) return <Loader />;
+
+		if (fetchError) return <p>Error while fetching data. Please refresh or try again later.</p>;
+		else {
+			return products.map((product) => <ProductCard product={product}/>);
+		}
+	};
+
+	const content = renderContent();
+
 	return (
 		<div>
-			<h1 className='font-bold text-2xl'>This is the home page</h1>
+			<h1 className='font-bold text-2xl'>Product List</h1>
+			{content}
 		</div>
 	);
 };
