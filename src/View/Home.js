575931bca68ddd8e477e0b9d95f9225af7186377
Nicolas Terra Maia <nicolasterramaia@gmail.com>
2020-12-06 19:40:15 -0300

    refactored to better deal with state changes

--- src/View/Home.js
+++ src/View/Home.js
@@ -4,36 +4,36 @@ import Loader from '../Components/Loader';
 import ProductCard from '../Components/ProductCard';
 
 const Home = () => {
-	const [products, setProducts] = useState([]);
-	const [loading, setLoading] = useState(true);
-	const [fetchError, setFetchError] = useState(false);
+	const [products, setProducts] = useState({
+		loading: true,
+		data: [],
+		error: false,
+	});
 
-    
+	const url = `https://5fccf355603c0c00164873c8.mockapi.io/products/?page=1&limit=10`;
+
 	useEffect(() => {
-        const url = `https://5fccf355603c0c00164873c8.mockapi.io/products/?page=1&limit=10`;
 		const fetchProductList = async () => {
-			setLoading(true);
+			setProducts((prevState) => {
+				return { ...prevState, loading: true };
+			});
 			try {
 				const response = await axios.get(url);
-				setLoading(false);
-				setProducts(response.data);
-				setFetchError(false);
+				setProducts({ loading: false, data: response.data, error: false });
 			} catch (error) {
-				setLoading(false);
-				setProducts(null);
-				setFetchError(true);
+				setProducts({ loading: false, data: null, error: true });
 			}
 		};
 
 		fetchProductList();
-	}, []);
+	}, [url]);
 
 	const renderContent = () => {
-		if (loading) return <Loader />;
+		if (products.loading) return <Loader />;
 
-		if (fetchError) return <p>Error while fetching data. Please refresh or try again later.</p>;
+		if (products.fetchError) return <p>Error while fetching data. Please refresh or try again later.</p>;
 		else {
-			return products.map((product) => <ProductCard product={product}/>);
+			return products.data.map((product) => <ProductCard product={product} />);
 		}
 	};
 
