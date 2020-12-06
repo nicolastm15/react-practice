a1402ab2f08e934963c3ecf18e0e104ca86624d3
Nicolas Terra Maia <nicolasterramaia@gmail.com>
2020-12-06 10:25:33 -0300

    tailwindcss and react-spring added to project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {backgroundColor:{'black-t-50': 'rgba(0,0,0,0.5)'}},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
